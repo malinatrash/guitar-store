3137ee356a900f48caebf139c00c28a14ab3d408
Pavel <malinatrashh@gmail.com>
2023-09-12 13:38:28 +0800

    fix logo on mobile

--- src/components/ui/guitarStoreLogo.tsx
+++ src/components/ui/guitarStoreLogo.tsx
@@ -14,13 +14,13 @@ const GuitarStoreLogo: FC<Props> = ({ onClick }) => {
 			onClick={onClick}
 		>
 			<img
-				className={`bg-slate-50 w-28 mobile:w-20 self-center mobile:h-20 mobile:object-fill rounded-full transition-all  ${
+				className={`bg-slate-50 w-28 mobile:w-16 self-center mobile:h-16 mobile:object-fill rounded-full transition-all  ${
 					isHover && 'opacity-50 animate-spin'
 				}`}
 				src='/assets/logo.png'
 				alt='logo'
 			/>
-			<div className='flex flex-col justify-center ml-5'>
+			<div className='flex flex-col justify-center ml-5 mobile:ml-0'>
 				<span
 					className={`transition-all text-slate-50 text-3xl mobile:text-[0] ${
 						isHover && 'opacity-50'
