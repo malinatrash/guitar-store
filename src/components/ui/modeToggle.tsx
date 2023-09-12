117e3dc331cbfa1487562a636fca1b36c7a7ba3f
Pavel <malinatrashh@gmail.com>
2023-09-12 14:31:43 +0800

    one more fix

--- src/components/ui/modeToggle.tsx
+++ src/components/ui/modeToggle.tsx
@@ -26,7 +26,7 @@ export function ModeToggle() {
 					>
 						<div>
 							<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 mobile:ml-[16%]' />
-							<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:top-[62px] mobile:ml-[16%]' />
+							<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:top-[62px] mobile:ml-[1%]' />
 							<span className='sr-only'></span>
 						</div>
 						Тема
