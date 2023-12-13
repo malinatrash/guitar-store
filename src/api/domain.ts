be8ee955e4274b7f42bf92382223db6cbff5cf88
Pavel <malinatrashh@gmail.com>
2023-12-13 23:54:36 +0800

    host

--- src/api/domain.ts
+++ src/api/domain.ts
@@ -1,1 +1,1 @@
-export const domain = 'http://127.0.0.1:8000/api/'
+export const domain = 'http://92.51.45.202:8000/api/'
--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const API_URL =
 	import.meta.env.MODE === 'mock'
 		? 'http://localhost:8000/api'
-		: 'http://localhost:8000/api/'
+		: 'http://92.51.45.202:8000/api/'
 
 export const api = createApi({
 	reducerPath: 'api',
